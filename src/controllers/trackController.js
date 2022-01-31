33636eec589417c6be79f1d4ce42d936b585e86c
AntonioCopete <antcopjim@gmail.com>
2022-01-31 18:16:17 +0100

    Feat: Add genre and name to track upload

--- src/controllers/trackController.js
+++ src/controllers/trackController.js
@@ -3,6 +3,7 @@ const db = require('../models');
 const { cloudinary } = require('../services/cloudinary');
 
 async function upload(req, res, next) {
+  const { name, genre } = req.body;
   try {
     const uploadedAudio = cloudinary.uploader.upload(req.files.track[0].path, {
       resource_type: 'video',
@@ -21,12 +22,26 @@ async function upload(req, res, next) {
     const audio = uploads[0];
     const image = uploads[1];
 
-    const createdTrack = await db.Track.create({
+    const trackSchema = {
       _id: audio.asset_id,
       url: audio.secure_url,
       userId: req.user._id,
       thumbnail: image.secure_url,
-    });
+      name: name,
+    };
+
+    const createdGenre = await db.Genre.findOne({ name: genre }).exec();
+    if (createdGenre) {
+      trackSchema.genre = createdGenre._id;
+    }
+    // let genreId = createdGenre._id;
+    if (!createdGenre) {
+      const newGenre = await db.Genre.create({ name: genre });
+      trackSchema.genre = newGenre._id;
+      //   genreId = newGenre._id;
+    }
+
+    const createdTrack = await db.Track.create(trackSchema);
 
     res.status(201).send({
       message: 'UPLOADED',
--- src/models/trackModel.js
+++ src/models/trackModel.js
@@ -1,21 +1,24 @@
 const mongoose = require('mongoose');
 
-const TrackSchema = new mongoose.Schema({
-  _id: { type: String },
-  name: { type: String, trim: true },
-  rating: { type: Number },
-  url: { type: String },
-  // popularity:{type:String},
-  thumbnail: { type: String },
-  //   createdAt: { type: ZonedDateTime },
-  //   released: { type: ZonedDateTime },
-  duration: { type: Number },
-  color: { type: String },
-  userId: { type: String },
-  // genre: { id: String, name: String },
-  // albums: [albumId: String],
-  // likedBy: [userId: String]
-});
+const TrackSchema = new mongoose.Schema(
+  {
+    _id: { type: String },
+    name: { type: String, trim: true },
+    rating: { type: Number },
+    url: { type: String },
+    // popularity:{type:String},
+    thumbnail: { type: String },
+    //   createdAt: { type: ZonedDateTime },
+    //   released: { type: ZonedDateTime },
+    duration: { type: Number },
+    color: { type: String },
+    userId: { type: String },
+    genre: { type: mongoose.Schema.Types.ObjectId, ref: 'tracks' },
+    // albums: [albumId: String],
+    // likedBy: [userId: String]
+  },
+  { timestamps: true }
+);
 
 const TrackModel = new mongoose.model('tracks', TrackSchema);
 
