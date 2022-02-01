8a67f5c089229613cbb54e502dfc51ae3a05efa9
AntonioCopete <antcopjim@gmail.com>
2022-02-01 11:31:46 +0100

    Change getGenres data response

--- src/controllers/genreController.js
+++ src/controllers/genreController.js
@@ -3,8 +3,11 @@ const db = require('../models');
 async function getGenres(req, res, next) {
   try {
     const genres = await db.Genre.find().select({ name: 1, _id: 0 });
+    const genreArr = genres.map((genre) => {
+      return genre.name;
+    });
 
-    res.status(200).send(genres);
+    res.status(200).send({ message: 'List of genres', data: genreArr });
   } catch (err) {
     next(err);
   }
