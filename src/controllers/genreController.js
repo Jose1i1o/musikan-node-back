8523b7936b0c37562dd60b8f9b81d7be87e36812
AntonioCopete <antcopjim@gmail.com>
2022-02-01 10:59:15 +0100

    Hotfix modeil import on genreController

--- src/controllers/genreController.js
+++ src/controllers/genreController.js
@@ -1,9 +1,9 @@
-const { Genre } = require('../models');
 const db = require('../models');
 
 async function getGenres(req, res, next) {
   try {
-    const genres = await Genre.find().select({ name: 1, _id: 0 });
+    const genres = await db.Genre.find().select({ name: 1, _id: 0 });
+
     res.status(200).send(genres);
   } catch (err) {
     next(err);
