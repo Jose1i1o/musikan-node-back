25aeac953c4d4af2add4cb008140a1e33ba00600
Jose1i1o <guadix88@gmail.com>
2022-02-08 21:14:33 +0100

    merge dev fixed

--- src/db/tracks.js
+++ src/db/tracks.js
@@ -3,20 +3,11 @@ const mongoose = require('mongoose');
 
 
 async function getBaseTracks() {
-<<<<<<< Updated upstream
-  let { _id: userId } = await db.User.findOne({}, { _id: 1 }).lean();
-  console.log(userId);
-  let genre = await db.Genre.findOne({}, { _id: 1 }).lean();
-  return [
-    {
-      _id : "1",
-=======
   let userId = await db.User.findOne({}, { _id: 1 }).lean();
   let genre = await db.Genre.findOne({}, { _id: 1 }).lean();
   return [
     {
-      _id: mongoose.Schema.Types.String,
->>>>>>> Stashed changes
+      _id: "1",
       name: "Song 1",
       artist: "Artist 1",
       rating: 5,
@@ -28,11 +19,7 @@ async function getBaseTracks() {
       genre: genre
     },
     {
-<<<<<<< Updated upstream
       _id : "2",
-=======
-      _id: mongoose.Schema.Types.String,
->>>>>>> Stashed changes
       name: "Song 2",
       artist: "Artist 2",
       rating: 5,
@@ -44,11 +31,7 @@ async function getBaseTracks() {
       genre: genre
     },
     {
-<<<<<<< Updated upstream
         _id : "3",
-=======
-        _id: mongoose.Schema.Types.String,
->>>>>>> Stashed changes
         name: "Song 3",
         artist: "Artist 3",
         rating: 5,
@@ -60,11 +43,7 @@ async function getBaseTracks() {
         genre: genre
     },
     {
-<<<<<<< Updated upstream
         _id : "4",
-=======
-        _id: mongoose.Schema.Types.String,
->>>>>>> Stashed changes
         name: "Song 4",
         artist: "Artist 4",
         rating: 5,
@@ -76,11 +55,7 @@ async function getBaseTracks() {
         genre: genre
     },
     {
-<<<<<<< Updated upstream
         _id : "5",
-=======
-        _id: mongoose.Schema.Types.String,
->>>>>>> Stashed changes
         name: "Song 5",
         artist: "Artist 5",
         rating: 5,
@@ -92,11 +67,7 @@ async function getBaseTracks() {
         genre: genre
     },
     {
-<<<<<<< Updated upstream
         _id : "6",
-=======
-        _id: mongoose.Schema.Types.String,
->>>>>>> Stashed changes
         name: "Song 6",
         artist: "Artist 6",
         rating: 5,
@@ -108,11 +79,7 @@ async function getBaseTracks() {
         genre: genre
     },
     {
-<<<<<<< Updated upstream
         _id : "7",
-=======
-        _id: mongoose.Schema.Types.String,
->>>>>>> Stashed changes
         name: "Song 7",
         artist: "Artist 7",
         rating: 5,
@@ -124,11 +91,7 @@ async function getBaseTracks() {
         genre: genre
     },
     {
-<<<<<<< Updated upstream
         _id : "8",
-=======
-        _id: mongoose.Schema.Types.String,
->>>>>>> Stashed changes
         name: "Song 8",
         artist: "Artist 8",
         rating: 5,
@@ -140,11 +103,7 @@ async function getBaseTracks() {
         genre: genre
     },
     {
-<<<<<<< Updated upstream
         _id : "9",
-=======
-        _id: mongoose.Schema.Types.String,
->>>>>>> Stashed changes
         name: "Song 9",
         artist: "Artist 9",
         rating: 5,
@@ -156,11 +115,7 @@ async function getBaseTracks() {
         genre: genre
     },
     {
-<<<<<<< Updated upstream
         _id : "10",
-=======
-        _id: mongoose.Schema.Types.String,
->>>>>>> Stashed changes
         name: "Song 10",
         artist: "Artist 10",
         rating: 5,
