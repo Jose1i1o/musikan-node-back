f8d4b20c121460dc939ef07e393092142c106e6a
Jose1i1o <guadix88@gmail.com>
2022-02-12 02:12:39 +0100

    feat feed playlists and iteratation over tracks and playlists completed

--- src/db/playlists.js
+++ src/db/playlists.js
@@ -8,7 +8,7 @@ async function getBasePlaylists() {
     
     // slice tracks in five arrays
     for (let i = 0; i < trackId.length; i++) {
-        if (i % 5 === 0) {
+        if (i % 10 === 0) {
             tracks.push(trackId.slice(i, i + 10));
         }
     }
@@ -22,71 +22,155 @@ async function getBasePlaylists() {
     let playlist3 = tracksIds.slice(2, 3);
     let playlist4 = tracksIds.slice(3, 4);
     let playlist5 = tracksIds.slice(4, 5);
+    let playlist6 = tracksIds.slice(5, 6);
+    let playlist7 = tracksIds.slice(6, 7);
+    let playlist8 = tracksIds.slice(7, 8);
+    let playlist9 = tracksIds.slice(8, 9);
+    let playlist10 = tracksIds.slice(9, 10);
+
 
 
     return [
     {
       userId: userId[Math.ceil(Math.random() * userId.length) - 1]._id,
-      name: "PLaylist 1",
+      name: "Road trip! All the masters I need!",
       tracks: playlist1[0].map(track => track.toString()),
       followedBy: userId,
       isFollowed: true,
       thumbnail:
-        "https://images.unsplash.com/photo-1632993952737-0c2897164db3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
+        "https://res.cloudinary.com/dmkdsujzh/image/upload/v1644625060/tracks-thumbnails-dev/car_yx6ld1.jpg",
       collaborative: false,
-      description: "The first playlist",
-      publicAccessible: false,
+      description: "Get your mic on with this beats. You are going to sing all the way down",
+      publicAccessible: true,
       primaryColor: "#fbdc00",
     },
     {
         userId: userId[Math.ceil(Math.random() * userId.length) - 1]._id,
-        name: "PLaylist 2",
+        name: "Motorcycle Mama!",
         tracks: playlist2[0].map(track => track.toString()),
         followedBy: userId,
         isFollowed: true,
         thumbnail:
-            "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80",
+            "https://res.cloudinary.com/dmkdsujzh/image/upload/v1644625058/tracks-thumbnails-dev/driving_g56eyw.jpg",
         collaborative: false,
-        description: "The second playlist",
-        publicAccessible: false,
+        description: "Take it easy on the wheels with this hits",
+        publicAccessible: true,
         primaryColor: "#fbdc00",
     },
     {
         userId: userId[Math.ceil(Math.random() * userId.length) - 1]._id,
-        name: "PLaylist 3",
+        name: "Chillout with the beats",
         tracks: playlist3[0].map(track => track.toString()),
         followedBy: userId,
         isFollowed: true,
         thumbnail:
-            "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80",
+            "https://res.cloudinary.com/dmkdsujzh/image/upload/v1644625056/tracks-thumbnails-dev/relax_coqdlx.jpg",
         collaborative: false,
-        description: "The third playlist",
-        publicAccessible: false,
+        description: "Stressed? Don´t worry the final project will soon be over. Relax in the meantime with this hits.",
+        publicAccessible: true,
         primaryColor: "#fbdc00",
     },
     {
         userId: userId[Math.ceil(Math.random() * userId.length) - 1]._id,
-        name: "PLaylist 4",
+        name: "Boogie with the beats",
         tracks: playlist4[0].map(track => track.toString()),
         followedBy: userId,
         isFollowed: true,
         thumbnail:
-            "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80",
+            "https://res.cloudinary.com/dmkdsujzh/image/upload/v1644625049/tracks-thumbnails-dev/boogie_rcl1zd.jpg",
         collaborative: false,
-        description: "The fourth playlist",
+        description: "Take the dance floor and try to get your groove on with this beats",
         publicAccessible: false,
         primaryColor: "#fbdc00",
     },
     {
         userId: userId[Math.ceil(Math.random() * userId.length) - 1]._id,
-        name: "PLaylist 5",
+        name: "Smile. You are beautiful",
         tracks: playlist5[0].map(track => track.toString()),
         followedBy: userId,
         isFollowed: true,
         thumbnail:
-            "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80",
+            "https://res.cloudinary.com/dmkdsujzh/image/upload/v1644624690/tracks-thumbnails-dev/smile_kpfzj8.jpg",
         collaborative: false,
-        description: "The fifth playlist",
+        description: "For those times when you are feeling down.",
+        publicAccessible: false,
+        primaryColor: "#fbdc00",
+    },
+    {
+        userId: userId[Math.ceil(Math.random() * userId.length) - 1]._id,
+        name: "Staying home",
+        tracks: playlist6[0].map(track => track.toString()),
+        followedBy: userId,
+        isFollowed: true,
+        thumbnail:
+            "https://res.cloudinary.com/dmkdsujzh/image/upload/v1644626587/tracks-thumbnails-dev/brennan-ehrhardt-HALe2SmkWAI-unsplash_c1yhnz.jpg",
+        collaborative: false,
+        description: "Feeling homey. Let´s keep it warm.",
+        publicAccessible: false,
+        primaryColor: "#fbdc00",
+    },
+    {
+        userId: userId[Math.ceil(Math.random() * userId.length) - 1]._id,
+        name: "Space hits!",
+        tracks: playlist7[0].map(track => track.toString()),
+        followedBy: userId,
+        isFollowed: true,
+        thumbnail:
+            "https://res.cloudinary.com/dmkdsujzh/image/upload/v1644626067/tracks-thumbnails-dev/fly_hx4xyp.jpg",
+        collaborative: false,
+        description: "Have no money for a space trip but you can have a space hit!",
+        publicAccessible: false,
+        primaryColor: "#fbdc00",
+    },
+    {
+        userId: userId[Math.ceil(Math.random() * userId.length) - 1]._id,
+        name: "Hits for the weekend",
+        tracks: playlist8[0].map(track => track.toString()),
+        followedBy: userId,
+        isFollowed: true,
+        thumbnail:
+            "https://res.cloudinary.com/dmkdsujzh/image/upload/v1644626096/tracks-thumbnails-dev/hits_nuk3qf.webp",
+        collaborative: false,
+        description: "Have no plans but will be happy to hear you are happy.",
+        publicAccessible: false,
+        primaryColor: "#fbdc00",
+    },
+    {
+        userId: userId[Math.ceil(Math.random() * userId.length) - 1]._id,
+        name: "Party time",
+        tracks: playlist9[0].map(track => track.toString()),
+        followedBy: userId,
+        isFollowed: true,
+        thumbnail:
+            "https://res.cloudinary.com/dmkdsujzh/image/upload/v1644626587/tracks-thumbnails-dev/amy-shamblen-lJt-3NUFng4-unsplash_z4icus.jpg",
+        collaborative: false,
+        description: "Have you finished your master project? Let´s party!",
+        publicAccessible: false,
+        primaryColor: "#fbdc00",
+    },
+    // {
+    //     userId: userId[Math.ceil(Math.random() * userId.length) - 1]._id,
+    //     name: "Old but gold",
+    //     tracks: playlist11[0].map(track => track.toString()),
+    //     followedBy: userId,
+    //     isFollowed: true,
+    //     thumbnail:
+    //         "https://res.cloudinary.com/dmkdsujzh/image/upload/v1644626590/tracks-thumbnails-dev/imani-bahati-ut67iFuoD2o-unsplash_nysrig.jpg",
+    //     collaborative: false,
+    //     description: "Oldies but goldies. Let´s get old and old.",
+    //     publicAccessible: false,
+    //     primaryColor: "#fbdc00",
+    // },
+    {
+        userId: userId[Math.ceil(Math.random() * userId.length) - 1]._id,
+        name: "Glitter",
+        tracks: playlist10[0].map(track => track.toString()),
+        followedBy: userId,
+        isFollowed: true,
+        thumbnail:
+            "https://res.cloudinary.com/dmkdsujzh/image/upload/v1644627361/tracks-thumbnails-dev/glitter_ru5qsp.jpg",
+        collaborative: false,
+        description: "Let´s get glittering.",
         publicAccessible: false,
         primaryColor: "#fbdc00",
     },
