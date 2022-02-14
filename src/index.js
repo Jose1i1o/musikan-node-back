189bfa5021466adec8cf4401a00e3fed444e6659
AntonioCopete <antcopjim@gmail.com>
2022-02-14 18:18:27 +0100

    fix port

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ const app = require('./server');
 const express = require('express');
 const { CONFIG } = require('./config/config');
 const connect = require('./db/connect');
-const { seedTracks, seedPlaylist, seedGenres, seedUsers  } = require('./db/seed');
+const {
+  seedTracks,
+  seedPlaylist,
+  seedGenres,
+  seedUsers,
+} = require('./db/seed');
 
 connect().then(async function onServerInit() {
   CONFIG.development.logger.info(`DB connected`);
@@ -13,7 +18,7 @@ connect().then(async function onServerInit() {
 });
 
 // port
-app.listen(CONFIG.development.app.PORT, () => {
+app.listen(CONFIG.development.app.PORT || 8080, () => {
   CONFIG.development.logger.info(
     `Server running at ${CONFIG.development.app.PORT}`
   );
